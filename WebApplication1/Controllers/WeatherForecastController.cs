c0cc7f4a82d8b160ab8f28b21739e3c6f58512c9
shortymai <121906745+shortymai@users.noreply.github.com>
2023-01-09 19:03:57 -0600

    1 city forecast model (#4)
    * update .gitignore
    
    * add CityForecast Model

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
---
+++ WebApplication1/Models/CityForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WebApplication1.Models
+{
+    public class CityForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int Temperature { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid Id { get; set; }
+    }
+}
