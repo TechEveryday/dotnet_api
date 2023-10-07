fe33e12d49e147e87f3b7ac5b6a1cb95e8a9b7d8
Alexander Philavong <alexander.philavong.0640@gmail.com>
2023-10-07 18:08:41 -0500

    Refactor, separate out ApiController into smaller classes

---
+++ DotnetApi/Controllers/AttributeController.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using DotnetApi.Services;
+
+namespace DotnetApi.Controllers
+{
+  [ApiController]
+  public class AttributeController : ControllerBase
+  {
+    private readonly ILogger<AttributeController> _logger;
+    private readonly AttributeService _attributeService;
+
+    public AttributeController(
+        ILogger<AttributeController> logger,
+        AttributeService attributeService
+    )
+    {
+      _logger = logger;
+      _attributeService = attributeService;
+    }
+
+    [HttpGet]
+    [Route("attribute/getByEntityId/{entityId}")]
+    public IActionResult GetAttributes(Guid entityId)
+    {
+      return Ok(_attributeService.get(entityId));
+
+    }
+
+    [HttpGet]
+    [Route("attribute/getById/{id}")]
+    public IActionResult GetAttributeById(Guid id)
+    {
+      try
+      {
+        return Ok(_attributeService.getById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    [Route("attribute/create")]
+    public IActionResult CreateAttribute([FromBody] Models.Attribute attribute)
+    {
+      try
+      {
+        _attributeService.create(attribute);
+        return Ok(attribute);
+      }
+      catch (Exception)
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError,
+            "Error creating new attribute");
+      }
+    }
+
+    [HttpPatch]
+    [Route("attribute/update")]
+    public IActionResult PatchAttribute([FromBody] Models.Attribute attribute)
+    {
+      try
+      {
+        _attributeService.update(attribute);
+        return Ok(attribute);
+      }
+      catch
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError,
+            "Error updating attribute");
+      }
+    }
+
+    [HttpDelete]
+    [Route("attribute/delete/{id}")]
+    public IActionResult DeleteAttribute(Guid id)
+    {
+      try
+      {
+        _attributeService.delete(id);
+        return Ok();
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+  }
+}
---
+++ DotnetApi/Controllers/RecordController.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using DotnetApi.Models;
+using DotnetApi.Services;
+
+namespace DotnetApi.Controllers
+{
+  [ApiController]
+  public class RecordController : ControllerBase
+  {
+    private readonly ILogger<RecordController> _logger;
+    private readonly RecordService _recordService;
+
+    public RecordController(
+        ILogger<RecordController> logger,
+        RecordService recordService
+    )
+    {
+      _logger = logger;
+      _recordService = recordService;
+    }
+
+    [HttpGet]
+    [Route("record")]
+    public IActionResult GetRecords([FromBody] Guid entityId)
+    {
+      return Ok(_recordService.get(entityId));
+
+    }
+
+    [HttpGet]
+    [Route("record/{id}")]
+    public IActionResult GetRecordById(int id)
+    {
+      try
+      {
+        return Ok(_recordService.getById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    [Route("record")]
+    public IActionResult CreateRecord([FromBody] Record record)
+    {
+      try
+      {
+        _recordService.create(record);
+        return Ok(record);
+      }
+      catch (Exception)
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError,
+            "Error creating new record");
+      }
+    }
+
+    [HttpPatch]
+    [Route("record")]
+    public IActionResult PatchRecord([FromBody] Record record)
+    {
+      try
+      {
+        _recordService.update(record);
+        return Ok(record);
+      }
+      catch
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError,
+            "Error updating record");
+      }
+    }
+
+    [HttpDelete]
+    [Route("record/{id}")]
+    public IActionResult DeleteRecord(int id)
+    {
+      try
+      {
+        _recordService.delete(id);
+        return Ok();
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+  }
+}
