9ae6f4918695bc4efbfbb60d5f7ad6ef73cb9c46
Alexander Philavong <alexander.philavong.0640@gmail.com>
2023-11-07 22:32:37 -0600

    add pk to entityrelationships

---
+++ DotnetApi/Migrations/20231108043046_PkGuidToEntityRelationships.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PkGuidToEntityRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "id",
+                table: "entity_relationship",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_entity_relationship",
+                table: "entity_relationship",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_entity_relationship",
+                table: "entity_relationship");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "entity_relationship");
+        }
+    }
+}
--- DotnetApi/Models/EntityRelationship.cs
+++ DotnetApi/Models/EntityRelationship.cs
@@ -5,12 +5,12 @@ using Microsoft.EntityFrameworkCore;
 namespace DotnetApi.Models
 {
   [Table("entity_relationship")]
-  [Keyless]
   public class EntityRelationship
   {
+    [Column("id")]
+    public Guid Id { get; set; }
     [Column("child_entity_id")]
     public Guid ChildEntityId { get; set; }
-
     [Column("entity_id")]
     public Guid EntityId { get; set; }
   }
